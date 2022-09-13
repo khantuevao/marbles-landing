39e714ac9873e6fc64124ccfd4ef5d31e4b8ecce
khantuevao <khantuevao@gmail.com>
2022-09-13 18:59:08 +0200

    Populate tournaments tabs

---
+++ tournaments.js
@@ -0,0 +1,128 @@
+addEventListener('DOMContentLoaded', (event) => {
+  live.click()
+})
+
+const container = document.getElementById('container')
+
+const live = document.getElementById('live')
+live.addEventListener('click', () => {
+  container.innerHTML = `<div id="tournaments">
+  <div class="tabName">Live Tournaments</div>
+  <div class="content">
+    <div class="head">
+      <a href="" id="title">Title</a>
+      <a href="" id="fund">Fund</a>
+      <a href="" id="buyin">Buy-in</a>
+    </div>
+    <div class="tournament">
+      <div class="title">Sunday Million Arena. Unlimited participants</div>
+      <div class="fund">$1,000,000 USDT</div>
+      <div class="buyin">$100</div>
+    </div>
+    <div class="tournament">
+      <div class="title">Wednesday Weekly. 1000 participants</div>
+      <div class="fund">$50,000 USDT</div>
+      <div class="buyin">$50</div>
+    </div>
+    <div class="tournament">
+      <div class="title">Daily Clash. 100 participants. Once a day at 16:00</div>
+      <div class="fund">$10,000 USDT</div>
+      <div class="buyin">$100</div>
+    </div>
+    <div class="tournament">
+      <div class="title">Fill & Start. 100 participants</div>
+      <div class="fund">$1,000 USDT</div>
+      <div class="buyin">$10</div>
+    </div>
+    <div class="tournament">
+      <div class="title">Fill & Start. 100 participants</div>
+      <div class="fund">$1,000 USDT</div>
+      <div class="buyin">$10</div>
+    </div>
+    <div class="tournament">
+      <div class="title">Fill & Start. 100 participants</div>
+      <div class="fund">$1,000 USDT</div>
+      <div class="buyin">$10</div>
+    </div>
+    <div class="tournament">
+      <div class="title">Fill & Start. 100 participants</div>
+      <div class="fund">$1,000 USDT</div>
+      <div class="buyin">$10</div>
+    </div>
+    <div class="tournament">
+      <div class="title">Fill & Start. 100 participants</div>
+      <div class="fund">$1,000 USDT</div>
+      <div class="buyin">$10</div>
+    </div>
+    <div class="tournament">
+      <div class="title">Fill & Start. 100 participants</div>
+      <div class="fund">$1,000 USDT</div>
+      <div class="buyin">$10</div>
+    </div>
+  </div>
+</div>`
+})
+
+const hot = document.getElementById('hot')
+hot.addEventListener('click', () => {
+  container.innerHTML = `<div id="tournaments">
+  <div class="tabName">Hot</div>
+  <div class="content">
+    <div class="head">
+      <a href="" id="title">Title</a>
+      <a href="" id="fund">Fund</a>
+      <a href="" id="buyin">Buy-in</a>
+    </div>
+    <div class="tournament">
+      <div class="title">Friday Happy Hours. Unlimited participants</div>
+      <div class="fund">$200,000 USDT</div>
+      <div class="buyin">$50</div>
+    </div>
+    <div class="tournament">
+      <div class="title">Daily Clash. 100 participants. Once a day at 16:00</div>
+      <div class="fund">$10,000 USDT</div>
+      <div class="buyin">$100</div>
+    </div>
+    <div class="tournament">
+      <div class="title">Fill & Start. 100 participants</div>
+      <div class="fund">$1,000 USDT</div>
+      <div class="buyin">$10</div>
+    </div>
+  </div>
+</div>`
+})
+
+const upcoming = document.getElementById('upcoming')
+upcoming.addEventListener('click', () => {
+  container.innerHTML = `<div id="tournaments">
+  <div class="tabName">Upcoming</div>
+  <div class="content">
+  <div class="head">
+      <a href="" id="title">Title</a>
+      <a href="" id="fund">Fund</a>
+      <a href="" id="buyin">Buy-in</a>
+    </div>
+    <div class="tournament">
+      <div class="title">Wednesday Weekly. 1000 participants</div>
+      <div class="fund">$50,000 USDT</div>
+      <div class="buyin">$50</div>
+    </div>
+    <div class="tournament">
+      <div class="title">Mid-week Madness. Unlimited participants</div>
+      <div class="fund">$80,000 USDT</div>
+      <div class="buyin">$30</div>
+    </div>
+    <div class="tournament">
+      <div class="title">Friday Happy Hours. Unlimited participants</div>
+      <div class="fund">$200,000 USDT</div>
+      <div class="buyin">$50</div>
+    </div>
+    <div class="tournament">
+      <div class="title">Sunday Million Arena. Unlimited participants</div>
+      <div class="fund">$1,000,000 USDT</div>
+      <div class="buyin">$100</div>
+    </div>
+  </div>
+</div>`
+})
+
